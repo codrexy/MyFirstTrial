1ddbbf1a95da67738e930e01fe863d7457c78249
vincentovie39 <vincentovie39@gmail.com>
2020-12-29 14:36:08 +0100

    finished services

---
+++ script/services.js
@@ -0,0 +1,23 @@
+function first() {
+    document.getElementById ("first").classList.toggle("show");
+}
+
+function second() {
+    document.getElementById ("second").classList.toggle("show");
+}
+
+function third() {
+    document.getElementById ("third").classList.toggle("show");
+}
+
+function fourth() {
+    document.getElementById ("fourth").classList.toggle("show");
+}
+
+function fifth() {
+    document.getElementById ("fifth").classList.toggle("show");
+}
+
+function sixth() {
+    document.getElementById ("sixth").classList.toggle("show");
+}
