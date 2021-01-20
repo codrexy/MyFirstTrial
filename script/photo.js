d22fba21f2b6b720f338db54f1787fbfc4ccdd5f
vincentovie39 <vincentovie39@gmail.com>
2021-01-20 11:41:20 +0100

    fixed photo gallery with javascript

--- script/photo.js
+++ script/photo.js
@@ -1,4 +1,23 @@
-// var toggleModal =  document.getElementById("modal").classList.toggle("hide");
-// function close() {
-//     document.getElementById("close").toggleModal;
-// }
+document.getElementsByTagName("img")[0].addEventListener('click', function(e) {
+    console.log('gmmm')
+})
+
+function first() {
+    let modal = document.getElementById("modal")
+    modal.classList.add('show')
+    // let imgBox = document.getElementsByClassName("img")[0];
+    // imgBox.addEventListener('click', function (e) {
+    //     console.log(e)
+    // })
+}
+
+
+// MODAL CLOSER
+let closer = document.getElementsByClassName('close')[0];
+closer.addEventListener('click', function(e){
+    document.getElementById("modal").classList.remove("show");
+})
+
+
+// let sect = document.getElementsByClassName('ph-section')[0]
+// console.log(sect.firstElementChild.outerHTML)
