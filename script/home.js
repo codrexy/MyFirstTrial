55586fceb4d939546388be992231372b46a7c74f
vincentovie39 <vincentovie39@gmail.com>
2021-01-21 12:23:22 +0100

    done nex and previous button

--- script/home.js
+++ script/home.js
@@ -5,3 +5,15 @@ function one() {
 function two() {
     document.getElementById("other").classList.toggle("show");
 }
+
+function first() {
+    document.getElementById ("first").classList.toggle("show");
+}
+
+function second() {
+    document.getElementById ("second").classList.toggle("show");
+}
+
+function third() {
+    document.getElementById ("third").classList.toggle("show");
+}
--- script/photo.js
+++ script/photo.js
@@ -1,23 +1,49 @@
-document.getElementsByTagName("img")[0].addEventListener('click', function(e) {
-    console.log('gmmm')
+// JQUERY BEGINS HERE
+$("document").ready(function() {
+
+    // INSERT IMG SRC TO MODAL
+    $('.img').click(function(){
+        let imgSrc = $(this).attr('src')
+        $('.modal-img').attr('src',imgSrc)
+    })
+
+    // MODAL CLOSER
+    $('.close').click(function(){
+        $('#modal').removeClass('show')
+        $('.nxt-cont').show();
+    })
+
+    // MODAL OPENER
+    $('.img').click(function() {
+        $('#modal').addClass('show')
+    })
+
+    // NEXT BUTTON AND PREVIOUS BUTTON
+    $('.presser').click(function() {
+        
+        let gottenurl = $(this).offsetParent().siblings('img').attr('src');
+        let splitedArray = gottenurl.split('/');
+        let arrayPos = splitedArray[splitedArray.length - 1];
+        let indexOf;
+        if($(this).hasClass('nxt-btn')){
+            indexOf = imgs.indexOf(arrayPos) + 1;            
+        }else{
+            indexOf = imgs.indexOf(arrayPos) - 1;
+        }
+        // if(indexOf == imgs.length -1){
+        //     $('.nxt-cont').hide();
+        // }else if(indexOf-1 == 1){
+        //     $('.prev-cont').hide();
+        // } 
+        let newImage = imgs[indexOf];
+        splitedArray.pop()
+        let newImgUrl = splitedArray.join('/')
+        console.log(imgs.length);
+        console.log(indexOf)
+        if(imgs.length != indexOf){
+            if(indexOf+1 != 0){
+                $('.modal-img').attr('src',newImgUrl + `/${newImage}`)
+            }
+        }
+    })
 })
-
-function first() {
-    let modal = document.getElementById("modal")
-    modal.classList.add('show')
-    // let imgBox = document.getElementsByClassName("img")[0];
-    // imgBox.addEventListener('click', function (e) {
-    //     console.log(e)
-    // })
-}
-
-
-// MODAL CLOSER
-let closer = document.getElementsByClassName('close')[0];
-closer.addEventListener('click', function(e){
-    document.getElementById("modal").classList.remove("show");
-})
-
-
-// let sect = document.getElementsByClassName('ph-section')[0]
-// console.log(sect.firstElementChild.outerHTML)
